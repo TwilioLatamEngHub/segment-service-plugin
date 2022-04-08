90a6e4719f47cf83446c0bb362701dd493953fcd
Gabriel Oliveira <goliveira@twilio.com>
2022-04-08 16:32:05 -0300

    Ignore Segment
    If the Segment Service isn't deployed, the plugin will simply deactivate Segment mode

--- plugins/plugin-customer-context/src/utils/SegmentUtil.js
+++ plugins/plugin-customer-context/src/utils/SegmentUtil.js
@@ -4,32 +4,40 @@ const manager = Manager.getInstance();
 class SegmentUtil {
   getPersonaData = async (userId) => {
     console.debug('Getting persona:', userId);
-    const fetchUrl = `${process.env.FLEX_APP_FUNCTIONS_BASE}/fetchPersona`;
-    const fetchBody = {
-      Token: manager.store.getState().flex.session.ssoTokenPayload.token,
-      idType: 'user_id',
-      limit: 100,
-      identifier: userId
-    };
-    const fetchOptions = {
-      method: 'POST',
-      body: new URLSearchParams(fetchBody),
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
-      }
-    };
-  
-    let data;
-    try {
-      const response = await fetch(fetchUrl, fetchOptions);
-      data = await response.json();
-      console.debug('Persona data:', data);
-    } catch (error) {
-      console.error('Failed to get template strings');
+    if(process.env.FLEX_APP_FUNCTIONS_BASE){
+        const fetchUrl = `${process.env.FLEX_APP_FUNCTIONS_BASE}/fetchPersona`;
+        const fetchBody = {
+            Token: manager.store.getState().flex.session.ssoTokenPayload.token,
+            idType: 'user_id',
+            limit: 100,
+            identifier: userId
+        };
+        const fetchOptions = {
+            method: 'POST',
+            body: new URLSearchParams(fetchBody),
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+            }
+        };
+    
+        let data;
+        try {
+            const response = await fetch(fetchUrl, fetchOptions);
+            data = await response.json();
+            console.debug('Persona data:', data);
+        } catch (error) {
+            console.error('Failed to get template strings');
+        }
+    
+        return data;
     }
-  
-    return data;
-  }
+
+    else {
+        console.log('No Segment Service Base URL. Segment Mode deactivated,');
+        return null;
+    };
+}
+
 }
 
 const segmentUtil = new SegmentUtil();
